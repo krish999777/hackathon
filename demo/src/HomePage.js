8abf72ddaf2fe931612fac409a3ed7e311ddac42
Avadhoot Savle <160419223+KalkulusSecurytas@users.noreply.github.com>
2025-09-16 12:05:13 +0530

    added

---
+++ demo/src/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './main/Navbar'
+import Footer from './main/Footer'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar/>
+
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ demo/src/main/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h3>Home works</h3>
+
+
+      <button>1 </button>
+      <button>2</button>
+      <button>3</button>
+
+    </div>
+  )
+}
+
+export default Home
